c0f131cd80bd96964b7997a51d708f3a28b10478
rikfo <akagi.ippo99@gmail.com>
2021-08-03 16:08:12 +0100

    adding more features

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import studentRoute from './routes/studentRouter.js';
 import levelRoute from './routes/levelRouter.js';
 import questionRoute from './routes/questionRouter.js';
+import errorController from './controllers/errorController.js';
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 app.use('/api/v1/users', studentRoute);
 app.use('/api/v1/questions', questionRoute);
 app.use('/api/v1/levels', levelRoute);
+
+app.use(errorController);
 
 export default app;
--- controllers/levelController.js
+++ controllers/levelController.js
@@ -1,4 +1,5 @@
 import { catchAsync } from '../utils/catchAsync.js';
+import ErrorHandler from '../utils/errorHandler.js';
 import Level from '../models/levelModel.js';
 
 // const getLevel = catchAsync(async (req, res) => {
@@ -30,8 +31,9 @@ const getLevelById = catchAsync(async (req, res, next) => {
   const level = await Level.findById(req.params.id);
   if (level.level > req.user.level.level) {
     return next(
-      new Error(
-        'you should finish the your current level in order to reach this level'
+      new ErrorHandler(
+        'you should finish the your current level in order to reach this level',
+        403
       )
     );
   }
